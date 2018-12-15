f9903193e0cb1630e8bb59c4bb1bdcaede6899a6
RobotCookie <juicyninja11@gmail.com>
2018-12-15 14:01:21 +0000

    Created Option To Launch Games, And Made It So Extensions Are Not Shown

--- main.py
+++ main.py
@@ -4,25 +4,38 @@
 
 import tkinter, time, sys, os, csv, winshell
 from win32com.client import Dispatch
+os.chdir("Games")
 def add():
-    pathed = str(input("Input The Game's Locations Ex: C:\Program Files\GameFun\gun.exe "))
-    pathed2 = str(input("Now Input That Again But Without The Exe Ex: C:\Program Files\GameFun "))
+    pathed = str(input("Input The Game's Locations Ex: C:\Program Files\GameFun\gun.exe: "))
     nameInput = str(input("What is the name of the game? "))
     name = (nameInput+".lnk")
-    path = os.path.join("Games", name)
+    path = os.path.join(name)
     target = (pathed)
-    wDir = (pathed2)
     icon = (pathed)
 
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(path)
     shortcut.Targetpath = target
-    shortcut.WorkingDirectory = wDir
     shortcut.IconLocation = icon
     shortcut.save()
+    default()
 def default():
-    os.chdir("Games")
+    print("========================GAME LIST=================================")
     dir = os.listdir()
-    for item in dir:
+    dirList = [x.split('.')[0] for x in dir]
+    for item in dirList:
         print (item)
+    print("==================================================================")
+    noError = True
+    while noError:
+        awaitAction()
+def awaitAction():
+    command = str(input("Type Run to launch a game to launch a game, or Add To Add A Game: "))
+    if command.lower() == ("run"):
+        program = str(input("Name The Game You Want To Run: "))+".lnk"
+        os.system("start"+" "+program)
+    elif command.lower() ==("add"):
+        add()
+    else:
+        awaitAction()
 default()
