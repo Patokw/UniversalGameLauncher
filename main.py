43c21feba892412a5bdd5d3f119e9a2732ed2e0f
RobotCookie <juicyninja11@gmail.com>
2018-12-15 13:41:28 +0000

    Created Shortcut Making Program

---
+++ main.py
@@ -0,0 +1,22 @@
+#Universal Game Launcher
+#RobotCookie
+#15.12.18
+
+import tkinter, time, sys, os, csv, winshell
+from win32com.client import Dispatch
+
+pathed = str(input("Input The Game's Locations Ex: C:\Program Files\GameFun\gun.exe "))
+pathed2 = str(input("Now Input That Again But Without The Exe Ex: C:\Program Files\GameFun "))
+nameInput = str(input("What is the name of the game? "))
+name = (nameInput+".lnk")
+path = os.path.join("Games", name)
+target = (pathed)
+wDir = (pathed2)
+icon = (pathed)
+
+shell = Dispatch('WScript.Shell')
+shortcut = shell.CreateShortCut(path)
+shortcut.Targetpath = target
+shortcut.WorkingDirectory = wDir
+shortcut.IconLocation = icon
+shortcut.save()
