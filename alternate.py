35efadd95e5455bdadcc05f0320b0f20a2ed6f54
RobotCookie <juicyninja11@gmail.com>
2018-12-23 19:50:13 +0000

    Removed 'remove' function as it no longer works with the CSV Method.

--- alternate.py
+++ alternate.py
@@ -29,17 +29,8 @@ def awaitAction():
     command = str(input(">>> "))
     if command.lower() == ("add"):
         add()
-    elif command.lower() == ("remove"):
-        try:
-            filea = str(input("What Game Do You Want To Remove? "))
-            program = (filea+".lnk")
-            os.remove(program)
-            default()
-        except:
-            print("Invalid Game Name")
-            awaitAction()
     elif command.lower() == ("help"):
-        print("<Add - Adds A Game To The List> <Remove - Removes A Game From The List> <GameName - Launches That Game>:")
+        print("<Add - Adds A Game To The List> <GameName - Launches That Game>:")
         awaitAction()
     else:
         file2 = open("games.csv","r")
